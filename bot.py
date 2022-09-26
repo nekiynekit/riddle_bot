10942f3e5b078aa82e28ffe516e573202905a787
Nikita Romanov <nik-romanof@yandex.ru>
2022-09-26 16:06:39 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,39 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.reply_keyboard import ReplyKeyboardMarkup, KeyboardButton
+from thinker import RuT5SmallModel
+
+API_TOKEN = '...'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+current_thinker = RuT5SmallModel()
+
+
+@dp.message_handler(commands=['start'])
+async def greetings(message: types.Message):
+    await message.reply('Привет, я Думатель. Сыграем?')
+
+@dp.message_handler(commands=['config'])
+async def configuration(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    rut5_button = KeyboardButton('rut5-small')
+    keyboard.add(rut5_button)
+    await message.answer("Выберите модель с которой хотите сыграть",\
+         reply_markup=keyboard)
+
+@dp.message_handler()
+async def guess_the_ridlde(message: types.Message):
+    riddle = message.text
+    if riddle == 'rut5-small':
+        global current_thinker
+        current_thinker = RuT5SmallModel()
+        await message.reply('Вы играете в загадки с rut5-small!')
+    await message.reply(f'{message.text} - 42!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
---
+++ thinker.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from torch import load
+
+
+class RuT5SmallModel(object):
+
+    def __init__(self):
+        self.model_name = 'cointegrated/rut5-small'
+        self.prefix = 'отгадай: '
+        self.ans_pref = 'я думаю отгадка - '
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = load('trained_lm.pth')
+
+    def preprocess_function(self, examples):
+        inputs = [self.prefix + doc or '' for doc in examples["riddle"]]
+        model_inputs = self.tokenizer(inputs, max_length=1024, truncation=True)
+        with self.tokenizer.as_target_tokenizer():
+            labels = self.tokenizer([self.ans_pref + doc for doc in examples["answer"]], max_length=128, truncation=True)
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+
+    def guess_the_riddle(self, sample):
+        tokens = self.tokenizer('guess: ' + sample, return_tensors='pt').input_ids
+        outputs = self.model.generate(tokens)
+        prediction = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return prediction
